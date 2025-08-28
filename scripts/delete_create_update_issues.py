a84221ca0cac7b20adcbec9405acd43355f60b52
Arthur Lee <arthur.lee@corp.springfinancial.ca>
2025-08-28 16:40:07 -0700

    WIP

--- scripts/delete_create_update_issues.py
+++ scripts/delete_create_update_issues.py
@@ -148,7 +148,8 @@ def create_issue_for_package(repo, pkg_name, pkg_alerts):
     issue_title = f"Security vulnerability detected: {pkg_name}"
     alert_sections = [format_alert_section(alert) for alert in pkg_alerts]
     body = f"This issue tracks security vulnerabilities detected by Dependabot for \
-**{pkg_name}**. We need to update the packages, and apply code fixes where necessary in the application.\n\n"
+**{pkg_name}**. We need to update the packages, and apply code fixes where necessary in the application. \
+Pin the package version to the latest version compatible with the application code.\n\n"
     body += "\n".join(alert_sections)
 
     severities = set(
